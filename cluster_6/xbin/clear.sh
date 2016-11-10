c7e536a4b7801598617650373b3e9fae663a6eb0
xushjie1987 <xsj2013@foxmail.com>
2016-11-10 18:07:12 +0800

    x201

---
+++ cluster_6/xbin/clear.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm -rf /data/mysql/cluster_6/tmps/*
+rm -rf /data/mysql/cluster_6/logs/binlog/*
+rm -rf /data/mysql/cluster_6/pids/*
+rm -rf /data/mysql/cluster_6/logs/error/*
+rm -rf /data/mysql/cluster_6/logs/general/*
+rm -rf /data/mysql/cluster_6/logs/slow/*
+rm -rf /data/mysql/cluster_6/sock/*
+
